934cb4c92798590ed43ae11a6f668db13c5694ac
TeamAnsh <130488642+TeamAnsh@users.noreply.github.com>
2023-06-04 19:04:49 +0530

    Update config.py

--- VipX/config.py
+++ VipX/config.py
@@ -31,7 +31,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "VIP MUSIC")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Anshux")
 
 # Your User ID.
 OWNER_ID = list(
