1d2dc5feb0de99679aa8b51ecb07cddee375a630
TeamAnsh <130488642+TeamAnsh@users.noreply.github.com>
2023-06-04 21:34:28 +0530

    Update config.py

--- VipX/config.py
+++ VipX/config.py
@@ -53,7 +53,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC",
+    "https://github.com/TeamAnsh/Anshux",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
